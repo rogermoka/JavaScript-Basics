aab0502e2e388fc364f229c26586c49034129f0b
rogermoka <roger.mokarzel@gmail.com>
2018-10-19 18:31:30 -0300

    39. 12- Exercise- FizzBuzz

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-function MaximumOfTwo (x, y){
-    return (x > y) ? x : y;
+
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+const output = fizzBuzz('16asdasd');
+console.log(output);
+
+function fizzBuzz(input) {
+
+    if (isNaN(input)){
+        return 'Not a number'
+    }
+    
+    if (input % 3 === 0 && input % 5 === 0){
+        return 'FizzBuzz'
+    }
+    
+    if (input % 3 === 0){
+        return 'Fizz'
+    }
+
+    if (input % 5 === 0){
+        return 'Buzz'
+    }
+    
+    return input
 }
-
-console.log(MaximumOfTwo(10,90));
-
-
-
